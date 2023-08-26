5a1695af316f1e26be24b90cc9992e937bae7548
henriqueweege <henriqueweege@gmail.com>
2023-08-26 12:58:43 -0300

    adicionando configuração para redirecionar para https

--- Exercicio/Application/Program.cs
+++ Exercicio/Application/Program.cs
@@ -1,4 +1,5 @@
 using DependencyRoomBooking.Extensions;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,11 @@ builder.Services.AddInfrastructureServices();
 builder.Services.AddMediatR();
 builder.Services.AddSwagger();
 builder.Services.AddControllers();
-
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.RedirectStatusCode = (int)HttpStatusCode.Redirect;
+    options.HttpsPort = 5001;
+});
 var app = builder.Build();
 
 app.UseHttpsRedirection();
